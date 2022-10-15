e9f0719dfae08a419c7169c8f36b87f6bb2888de
Ayoub Aitouna <aitounaayoub05@gmail.com>
2022-10-15 14:53:28 +0000

    Update 3 files

--- src/App.js
+++ src/App.js
@@ -3,13 +3,29 @@ import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
-import { Home, Auth } from "./component";
+import { Home, Auth, Partner, Admins } from "./component";
+import { BrowserRouter, Routes, Route, Link, Outlet } from "react-router-dom";
+
 function App() {
   return (
     <div className="w-full h-[100vh]">
       <Home />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Auth />} />
+          <Route path="/home" element={<Home />}>
+            <Route
+              path="rejected"
+              element={<Partner selectedStatus={"accepted"} />}
+            />
+            <Route
+              path="accepted"
+              element={<Partner selectedStatus={"rejected"} />}
+            />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
-
 export default App;
