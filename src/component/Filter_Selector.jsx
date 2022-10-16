eeabc19306ba9a69b2d482b86579652b705e2a16
Ayoub Aitouna <aitounaayoub05@gmail.com>
2022-10-16 12:16:08 +0000

    Update Admins.jsx and Filter_Selector.jsx

--- src/component/Filter_Selector.jsx
+++ src/component/Filter_Selector.jsx
@@ -17,7 +17,7 @@ const Filter_Selector = ({
       </label>
       <select
         value={Filter}
-        onChange={(value) => setFilter(value)}
+        onChange={(e) => setFilter(e.target.value)}
         id={title}
         class="h-[40px] bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500
 		  focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400
