d3bd608cb0a8767fbc90e56cada031f93a103ca0
Ayoub Aitouna <aitounaayoub05@gmail.com>
2022-10-21 11:06:22 +0000

    Update TaskSearch.js

--- src/component/TaskSearch.js
+++ src/component/TaskSearch.js
@@ -40,7 +40,70 @@ const DataRow = ({ item, index, onClick = () => {} }) => {
 };
 
 const TaskSearch = () => {
-  return <div></div>;
+  return (
+    <div class="flex flex-col  border-[1px] my-10 border-gray-200 rounded-lg ">
+      <div class="overflow-x-auto sm:-mx-6 lg:-mx-8">
+        <div class="py-2 inline-block min-w-full sm:px-6 lg:px-8">
+          <div class="overflow-hidden">
+            <table class="min-w-full">
+              <thead class="bg-white border-b">
+                <tr>
+                  <th
+                    scope="col"
+                    class="text-sm font-medium text-gray-900 px-6 py-4 text-left"
+                  >
+                    #
+                  </th>
+                  <th
+                    scope="col"
+                    class="text-sm font-medium text-gray-900 px-6 py-4 text-left"
+                  >
+                    partner name
+                  </th>
+                  <th
+                    scope="col"
+                    class="text-sm font-medium text-gray-900 px-6 py-4 text-left"
+                  >
+                    partner status
+                  </th>
+                  <th
+                    scope="col"
+                    class="text-sm font-medium text-gray-900 px-6 py-4 text-left"
+                  >
+                    manager name
+                  </th>
+                  <th
+                    scope="col"
+                    class="text-sm font-medium text-gray-900 px-6 py-4 text-left"
+                  >
+                    ville name
+                  </th>
+                  <th
+                    scope="col"
+                    class="text-sm font-medium text-gray-900 px-6 py-4 text-left"
+                  >
+                    Edite
+                  </th>
+                </tr>
+              </thead>
+              <tbody>
+                {Data.map((item, index) => (
+                  <>
+                    <DataRow
+                      key={item.id}
+                      item={item}
+                      index={index}
+                      onClick={() => OnSelect(item)}
+                    />
+                  </>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default TaskSearch;
