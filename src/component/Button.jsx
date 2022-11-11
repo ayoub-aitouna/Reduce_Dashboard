cd465fa0a6a08b18b9f030ede347d05a0d4ca7df
Ayoub Aitouna <aitounaayoub05@gmail.com>
2022-11-11 02:23:43 +0000

    Update 3 files

--- src/component/Button.jsx
+++ src/component/Button.jsx
@@ -5,16 +5,18 @@ function Button({
   Icon = () => <></>,
   OnClick = () => {},
   style = "",
+  type = "",
 }) {
   return (
-    <div
+    <button
+      type="submit"
       onClick={() => OnClick()}
       className={`flex flex-row justify-center items-center w-[520px] text-center
 	   h-[65px] p-[40px] gap-[8px] bg-[#2E5CFF] rounded-[12px] flex-none flex-grow-0 order-[0] max-w-full text-white cursor-pointer ${style}`}
     >
       <Icon />
       <p>{title}</p>
-    </div>
+    </button>
   );
 }
 export default Button;
