ac2e8195034a43242c0d0dbc97148f495af6bed8
Ayoub Aitouna <aitounaayoub05@gmail.com>
2022-11-12 03:11:14 +0000

    s

--- src/component/Popups/PartnerInfo.jsx
+++ src/component/Popups/PartnerInfo.jsx
@@ -51,6 +51,9 @@ const PartnerInfoRender = ({ item }) => {
           </thead>
           <tbody>
             <DataRow title={"#"} data={item.id} />
+            <DataRow title={"Submited At"} data={`${new Date(item.created_date).getDate()}/${new Date(
+          item.created_date
+        ).getMonth()}/${new Date(item.created_date).getFullYear()}`} />
             <DataRow
               title={"Logo"}
               Render={() => {
